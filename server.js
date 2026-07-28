384f7e49aac606b9876e8833baf6bb0a20aecc82
Gad Malik <gadmalik423@gmail.com>
2026-07-28 15:39:33 +0200

    Mon commit

--- server.js
+++ server.js
@@ -208,7 +208,7 @@ app.post("/updateprofile", async (req, res) => {
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(5001, () => {
+    console.log("Server is running on port 5001");
 });
 
